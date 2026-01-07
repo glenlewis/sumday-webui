4afba5cbc51133197c9d4a3af2576070d39d26a2
Glen Lewis <kisa@kisa.com>
2026-01-07 17:24:19 +0000

    HTTPS and Reverse Proxy Configuration

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import config
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -11,6 +12,16 @@ def create_app(config_name='default'):
     """Application factory pattern"""
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    # Configure app to work behind reverse proxy (Nginx)
+    # This tells Flask to trust the X-Forwarded-* headers from Nginx
+    app.wsgi_app = ProxyFix(
+        app.wsgi_app,
+        x_for=1,
+        x_proto=1,
+        x_host=1,
+        x_prefix=1
+    )
 
     # Initialize extensions
     db.init_app(app)
--- config.py
+++ config.py
@@ -14,7 +14,7 @@ class Config:
     AUTH0_CLIENT_ID = os.environ.get('AUTH0_CLIENT_ID')
     AUTH0_CLIENT_SECRET = os.environ.get('AUTH0_CLIENT_SECRET')
     AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
-    AUTH0_CALLBACK_URL = os.environ.get('AUTH0_CALLBACK_URL') or 'http://classic.kisa.com:5555/callback'
+    AUTH0_CALLBACK_URL = os.environ.get('AUTH0_CALLBACK_URL') or 'https://classic.kisa.com/callback'
     AUTH0_AUDIENCE = f"https://{os.environ.get('AUTH0_DOMAIN')}/userinfo"
 
 
